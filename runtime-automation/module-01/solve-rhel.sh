6b080edd45362c2a1f09b42ffbedc67e0d84b649
Nate Lager <nlager@redhat.com>
2026-03-23 13:54:11 -0400

    some tweaks, and removing gitweb

--- runtime-automation/module-01/solve-rhel.sh
+++ runtime-automation/module-01/solve-rhel.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Module 01 is an introduction - just run the example command from the content
+ls -l /etc
 echo "Solved module called module-01" >> /tmp/progress.log
--- runtime-automation/module-02/setup-rhel.sh
+++ runtime-automation/module-02/setup-rhel.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 echo "Starting module called module-02" >> /tmp/progress.log
+echo "Making sure our problem exists" >> /tmp/progress.logs
+chcon -t default_t -R /var/www/html
+chown -R 1002:1002 /var/www/html
