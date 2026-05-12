53ad5249166b808b2d2b07a8a6c8d19d1c897390
Mitesh The Mouse <44154255+miteshget@users.noreply.github.com>
2026-05-12 23:56:38 +0530

    Add exit command at the end of setup-rhel.sh
    Added exit command to ensure script termination.

--- runtime-automation/module-02/setup-rhel.sh
+++ runtime-automation/module-02/setup-rhel.sh
@@ -3,3 +3,4 @@ echo "Starting module called module-02" >> /tmp/progress.log
 echo "Making sure our problem exists" >> /tmp/progress.logs
 chcon -t default_t -R /var/www/html
 chown -R 1002:1002 /var/www/html
+exit 0
